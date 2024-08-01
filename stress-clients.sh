d7cdd9dcd15a511ac61b6d0f8f5e3840879c8558
José Bollo <jose.bollo@iot.bzh>
2024-08-01 19:34:57 +0200

    Add stress scripts

---
+++ stress-clients.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+ROOT=$(dirname $0)
+echo ROOT=$ROOT
+
+AFB=afb-binder
+CLI="afb-client -e"
+PORT=12345
+
+sync="-s"
+count=10
+null=false
+eval set -- $(getopt -o c:np:P:s -l count:,null,pipe:,port:,sync -- "$@") || exit
+while true
+do
+	case "$1" in
+	-c|--count)
+		if ! test "$2" -gt 0 2>/dev/null; then
+			echo "error: $2 is not a valid count" >&2
+			exit 1
+		fi
+		count="$2"
+		shift 2
+		;;
+	-n|--null)
+		null=true
+		shift
+		;;
+	-p|--pipe)
+		sync="-p $2"
+		shift 2
+		;;
+	-P|--port)
+		PORT="$2"
+		shift 2
+		;;
+	-s|--sync)
+		sync="-s"
+		shift
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
+CLI="$CLI $sync"
+
+OUT="$ROOT/stress-out-clients"
+echo rm $OUT.*
+rm $OUT.* 2> /dev/null
+
+if $null; then
+	OUT=/dev/null
+else
+	OUT="$OUT.%03d"
+fi
+
+commands() {
+cat << EOC
+hello ping true
+hello ping false
+hello pingnull true
+hello pingJsOn {"well":"formed","json":[1,2,3,4.5,true,false,null,"oups"]}
+hello subcall {"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}
+hello subcall {"api":"hello","verb":"subcall","args":{"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}}
+hello subcallsync {"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}
+hello subcallsync {"api":"hello","verb":"subcall","args":{"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}}
+hello subcall {"api":"hello","verb":"subcallsync","args":{"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}}
+hello subcallsync {"api":"hello","verb":"subcallsync","args":{"api":"hello","verb":"pingJson","args":[{"key1":"value1"}]}}
+hello eventadd {"tag":"ev1","name":"event-A"}
+hello eventadd {"tag":"ev2","name":"event-B"}
+hello eventpush {"tag":"ev1","data":[1,2,"hello"]}
+hello eventpush {"tag":"ev2","data":{"item":0}}
+hello eventsub {"tag":"ev2"}
+hello eventpush {"tag":"ev1","data":[1,2,"hello"]}
+hello eventpush {"tag":"ev2","data":{"item":0}}
+hello eventsub {"tag":"ev1"}
+hello subcall {"api":"hello","verb":"eventpush","args":{"tag":"ev1","data":[1,2,"hello"]}}
+hello subcall {"api":"hello","verb":"eventpush","args":{"tag":"ev2","data":{"item":0}}}
+hello subcallsync {"api":"hello","verb":"eventpush","args":{"tag":"ev1","data":[1,2,"hello"]}}
+hello subcallsync {"api":"hello","verb":"eventpush","args":{"tag":"ev2","data":{"item":0}}}
+hello eventunsub {"tag":"ev2"}
+hello eventpush {"tag":"ev1","data":[1,2,"hello"]}
+hello eventpush {"tag":"ev2","data":{"item":0}}
+hello eventdel {"tag":"ev1"}
+hello eventpush {"tag":"ev1","data":[1,2,"hello"]}
+hello eventpush {"tag":"ev2","data":{"item":0}}
+hello eventdel {"tag":"ev2"}
+EOC
+}
+
+r() {
+	while :; do commands; done |
+	$CLI "localhost:$PORT/api" > "$1" 2>&1
+}
+
+echo launch clients...
+declare -A children
+i=1
+while test $i -le $count; do
+	echo " + launch clients $i"
+	r $(printf "$OUT" $i) &
+	children[$!]=$!
+	i=$(expr $i + 1)
+done
+echo "done [${children[*]}]"
+
+trap "kill ${children[*]}" SIGTERM SIGINT SIGQUIT
+wait ${children[*]}
+
---
+++ stress-server.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+ROOT=$(dirname $0)
+cd ${ROOT:-.}
+ROOT=$(pwd)
+echo ROOT=$ROOT
+
+AFB=afb-binder
+HELLO=~/redpesk/redpesk-core/afb-binding/tutorials/v4/hello4.so
+PORT=12345
+TEST=test
+
+OUT=stress-out-server
+rm $OUT*
+
+tool=
+ws=false
+rpc=false
+blind=false
+traps=false
+eval set -- $(getopt -o wgsvbmct -l ws,gdb,strace,valgrind,blind,memcheck,callgrind,traps -- "$@") || exit
+while true
+do
+	case "$1" in
+	-w|--ws) ws=true; rpc=false; shift;;
+	-r|--rpc) ws=false; rpc=true; shift;;
+	-g|--gdb) tool=gdb; shift;;
+	-s|--strace) tool=strace; shift;;
+	-v|--valgrind) tool=valgrind; shift;;
+	-m|--memcheck) tool=memcheck; shift;;
+	-c|--callgrind) tool=callgrind; shift;;
+	-b|--blind) blind=true; shift;;
+	-t|--traps) traps=true; shift;;
+	--) shift; break;;
+	esac
+done
+
+case $tool in
+ gdb) cmd="$(type -p gdb) -ex run --args";;
+ valgrind) cmd="$(type -p valgrind) --leak-check=full";;
+ memckeck) cmd="$(type -p valgrind) --tool=memcheck --leak-check=full";;
+ callgrind) cmd="$(type -p valgrind) --tool=callgrind";;
+ strace) cmd="$(type -p strace) -tt -f -o $OUT.strace";;
+ *) cmd=;;
+esac
+
+OPTGLOB="--session-max=100 --workdir=$ROOT"
+if $traps; then
+  OPTGLOB="$OPTGLOB --trap-faults=no"
+fi
+OPTHTTP="--port=$PORT --roothttp=$TEST"
+OPTHELLO="--binding=$HELLO"
+OPTFRONT="$OPTGLOB --port=$PORT --roothttp=$TEST"
+OPTBACK="$OPTGLOB -q --no-httpd $OPTHELLO"
+
+
+if $rpc; then
+  CMD="$AFB $OPTBACK --rpc-server=unix:@afw/hello --exec $cmd $AFB $OPTFRONT --rpc-client=unix:@afw/hello"
+elif $ws; then
+  CMD="$AFB $OPTBACK --ws-server=unix:@afw/hello --exec $cmd $AFB $OPTFRONT --ws-client=unix:@afw/hello"
+else
+  CMD="$cmd $AFB $OPTFRONT $OPTHELLO"
+fi
+
+
+echo "launch: $CMD $@"
+case $tool in
+ gdb) $CMD "$@";;
+ *) if $blind; then exec $CMD "$@" > $OUT 2>&1; else exec $CMD "$@" 2>&1 | tee $OUT; fi
+esac
+
